f8211b4f56f07dd151e49ec471e047b17f6dfd6c
StrangeTim <strang3tim@gmail.com>
2016-12-13 16:11:51 -0800

    basic api running with dummy data

---
+++ server/routes/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const router = express.Router();
+
+// declare mongoose for connecting to MongoDB
+/*
+const mongoose = require('mongoose');
+var muser = process.env.MONGOUSER;
+var mpass = process.env.MONGOPASS;
+var mDB = "@ds131878.mlab.com:31878/pacc-dev"
+mongoose.connect('mongodb://'+muser+':'+mpass+mDB);
+*/
+
+// declare axios for dummy data
+const axios = require('axios');
+const axiosAPI = 'https://jsonplaceholder.typicode.com';
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+// Get all posts - axios dummy data for initial setup testing
+router.get('/posts', (req, res) => {
+  // Get posts from the mock api
+  // This should ideally be replaced with a service that connects to MongoDB
+  axios.get(`${axiosAPI}/posts`)
+    .then(posts => {
+      res.status(200).json(posts.data);
+    })
+    .catch(error => {
+      res.status(500).send(error)
+    });
+});
+
+module.exports = router;
